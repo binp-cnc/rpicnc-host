9ab14eb9c3afc344b9fa1e0e89a48f53317ff2ea
Alexey Gerasev <alexey.gerasev@gmail.com>
2018-01-10 16:43:53 +0700

    handle commands

--- start.py
+++ start.py
@@ -1,5 +1,16 @@
 import zmq
 import time
+import json
+import traceback
+import logging as log
+
+log.basicConfig(level=log.DEBUG, format="[%(levelname)s] %(message)s")
+
+
+def handle(req):
+	if req["type"] == "init":
+		return { "status": "ok" }
+	return { "status": "bad_type" }
 
 
 context = zmq.Context()
@@ -7,8 +18,16 @@ socket = context.socket(zmq.PAIR)
 socket.bind("tcp://*:5556")
 
 while True:
-	msg = socket.recv()
-	print("[recv] %s" % msg)
-	msg = b"host responce"
+	msg = socket.recv().decode("utf-8")
+	log.debug("received: '%s'" % msg)
+	req = json.loads(msg)
+
+	try:
+		res = handle(req)
+	except Exception:
+		log.warning("error handle request")
+		log.warning(traceback.format_exc())
+
+	msg = json.dumps(res).encode("utf-8")
 	socket.send(msg)
-	print("[send] %s" % msg)
+	log.debug("sent: '%s'" % msg)
