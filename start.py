833dbe93839c1e567378e7633dec5260faa71b2f
Alexey Gerasev <alexey.gerasev@gmail.com>
2018-01-22 18:52:04 +0700

    motion method

--- start.py
+++ start.py
@@ -15,26 +15,39 @@ libcnc = cdll.LoadLibrary(os.getcwd() + "/librpicnc/build/cnc.so")
 def handle(req):
 	global libcnc
 
+	res = {
+		"type": req["type"],
+		"status": "bad_type",
+	}
+
 	if req["type"] == "init":
 		libcnc.cnc_init()
-		return { "status": "ok" }
+		res["status"] = "ok"
 
 	if req["type"] == "quit":
 		libcnc.cnc_quit()
-		return { "status": "ok" }
+		res["status"] = "ok"
 
 	elif req["type"] == "scan":
 		if req["axis"] == "x":
-			libcnc.cnc_scan_x()
+			s = libcnc.cnc_scan_x()
 		elif req["axis"] == "y":
-			libcnc.cnc_scan_y()
-		return { "status": "ok" }
+			s = libcnc.cnc_scan_y()
+		res.update({
+			"axis": req["axis"],
+			"size": s,
+			"pos": 0,
+			"status": "ok"
+		})
 
-	elif req["type"] == "center":
-		libcnc.cnc_center()
-		return { "status": "ok" }
+	elif req["type"] == "move":
+		libcnc.cnc_move(*(req["pos"] + req["vel"] + req["acc"]))
+		res.update({
+			"pos": req["pos"],
+			"status": "ok"
+		})
 
-	return { "status": "bad_type" }
+	return res
 
 
 context = zmq.Context()
@@ -43,6 +56,8 @@ socket.bind("tcp://*:5556")
 
 poller = zmq.Poller()
 poller.register(socket, zmq.POLLOUT)
+
+log.info("server started")
 
 while True:
 	msg = socket.recv().decode("utf-8")
@@ -57,8 +72,10 @@ while True:
 
 	msg = json.dumps(res).encode("utf-8")
 	
-	if socket in dict(poller.poll(4000)):
+	if socket in dict(poller.poll(5000)):
 		socket.send(msg)
 		log.debug("sent: '%s'" % msg.decode("utf-8"))
 	else:
 		log.error("cannot send response")
+
+log.info("server stopped")
